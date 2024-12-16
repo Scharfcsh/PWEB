8f44adc9abe3935b4e4725d95cc3cb858c38da94
Scharfcsh <amanadhikari2003@gmail.com>
2024-12-16 12:28:34 +0530

    Vercel Analytics integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Backgroung from "./pages/Backgroung";
 import Hero from "./pages/Hero";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
-      <div className="relative overflow-hidden">
-        <Backgroung />
-        <Hero />
-      </div>
+      <Analytics>
+        <div className="relative overflow-hidden">
+          <Backgroung />
+          <Hero />
+        </div>
+      </Analytics>
     </>
   );
 }
