b8e5db6c719fe4c1963fefbbc58519f3d9b5cd51
Scharfcsh <amanadhikari2003@gmail.com>
2025-01-08 22:29:39 +0530

    inspect element revoked

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import React, { useEffect } from "react";
 import Backgroung from "./pages/Backgroung";
 import Hero from "./pages/Hero";
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 123) {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.key === 'C') {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.key === 'J') {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.key === 'U') {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
       <div className="relative overflow-hidden">
@@ -13,4 +40,5 @@ function App() {
     </>
   );
 }
+
 export default App;
